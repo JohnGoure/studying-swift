90950f2df17535a820c51116526d9cde1552b43b
JohnGoure <johnsgithub@gmail.com>
2018-05-20 19:32:46 -0500

    Updated the image of the Image view. Made 2 outlets in the view controller, an image view outlet and a scroll view outlet. Assigned the Viewcontroller as a UISCrollViewDelegate and added the functionality for being a delegate of UIScrollView, scrollView.delegate = self. Implemented a function called updateZoomFor that has a size parameter. UpdateZoomFor computes the width and height of the image view compared to the views bounds. Then the scale constant uses the min method to find which value is smaller and uses the smaller value. Then the minimumZoomScale is set on the scrollView to be the scale value. Lastly the viewForZooming method is implemented and it returns the imageview object.

--- ISpy/ISpy/ViewController.swift
+++ ISpy/ISpy/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
     }
-
+    
+    @IBOutlet weak var behanceImageView: UIImageView!
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return behanceImageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / behanceImageView.bounds.width
+        let heightScale = size.height / behanceImageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
